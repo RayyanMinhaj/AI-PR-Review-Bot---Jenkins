e933246d27f5d1835ccef367bbf254ef148d06e3
RayyanMinhaj <81464495+RayyanMinhaj@users.noreply.github.com>
2024-10-02 17:14:44 +0500

    Update notebook.py

--- notebook.py
+++ notebook.py
@@ -19,53 +19,54 @@ def display_todo_list(todo_list):
     """Display the current todo list."""
     if not todo_list:
         print("Your todo list is empty!")
-        return
-    print("Your Todo List:")
-    for index, task in enumerate(todo_list, start=1):
-        print(f"{index}. {task}")
+    else:
+        print("Your Todo List:")
+        for index, task in enumerate(todo_list, start=1):
+            print(f"{index}. {task}")
 
-def add_task(todo_list, task):
+def add_task(todo_list):
     """Add a task to the todo list."""
+    task = input("Enter a task: ")
     todo_list.append(task)
     print(f'Task "{task}" added to your todo list.')
+    save_todo_list(todo_list)
 
-def remove_task(todo_list, index):
+def remove_task(todo_list):
     """Remove a task from the todo list by index."""
-    if 0 <= index < len(todo_list):
-        removed_task = todo_list.pop(index)
-        print(f'Task "{removed_task}" removed from your todo list.')
-    else:
-        print("Invalid task number.")
+    try:
+        index = int(input("Enter task number to remove: ")) - 1
+        if 0 <= index < len(todo_list):
+            removed_task = todo_list.pop(index)
+            print(f'Task "{removed_task}" removed from your todo list.')
+            save_todo_list(todo_list)
+        else:
+            print("Invalid task number.")
+    except ValueError:
+        print("Please enter a valid number.")
 
 def main():
     """Main function to run the todo list application."""
     todo_list = load_todo_list()
-    
+    options = {
+        '1': ("View Todo List", lambda: display_todo_list(todo_list)),
+        '2': ("Add Task", lambda: add_task(todo_list)),
+        '3': ("Remove Task", lambda: remove_task(todo_list)),
+        '4': ("Exit", None)
+    }
+
     while True:
         print("\nOptions:")
-        print("1. View Todo List")
-        print("2. Add Task")
-        print("3. Remove Task")
-        print("4. Exit")
-        
+        for key, (desc, _) in options.items():
+            print(f"{key}. {desc}")
+
         choice = input("Choose an option: ")
-        
-        if choice == '1':
-            display_todo_list(todo_list)
-        elif choice == '2':
-            task = input("Enter a task: ")
-            add_task(todo_list, task)
-            save_todo_list(todo_list)
-        elif choice == '3':
-            try:
-                index = int(input("Enter task number to remove: ")) - 1
-                remove_task(todo_list, index)
-                save_todo_list(todo_list)
-            except ValueError:
-                print("Please enter a valid number.")
-        elif choice == '4':
-            print("Exiting the application. Goodbye!")
-            break
+
+        if choice in options:
+            if choice == '4':
+                print("Exiting the application. Goodbye!")
+                break
+            else:
+                options[choice][1]()
         else:
             print("Invalid option. Please try again.")
 
